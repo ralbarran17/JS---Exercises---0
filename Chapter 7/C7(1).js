def4961783b654774d61675e19dc0ee4989ae2c7
Rodrigo Albarran <87443584+ralbarran17@users.noreply.github.com>
2021-10-15 16:49:15 -0500

    Add files via upload

---
+++ Chapter 7/C7(1).js
@@ -0,0 +1,10 @@
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+  
+  function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
+  
+  console.log(rgbToHex(0, 51, 255)); // #0033ff
